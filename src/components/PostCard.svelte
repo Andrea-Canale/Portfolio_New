774e5e22d68ba5a4cb6af10ce5acb64fbc435bf4
andreock <vivanco11.ac@gmail.com>
2024-04-09 09:16:38 +0200

    Use grid in the index

--- src/components/PostCard.svelte
+++ src/components/PostCard.svelte
@@ -1,25 +1,21 @@
 <script>
-    export let title;
-    export let href;
+  export let title;
+  export let href;
 </script>
-<div class="card_size">
-<div
-  class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-  <h5
-    class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-    {title}
-  </h5> 
-  <a href={href}>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-	Leggi	
-    </button> 
-    </a>
-</div>
-</div>
-<style>
-    .card_size {
-	margin-left: 30em;
-	margin-right: 30em;
-    }
-</style>
 
+<div
+  class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+>
+  <h5
+    class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50"
+  >
+    {title}
+  </h5>
+  <a {href}>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Leggi
+    </button>
+  </a>
+</div>
